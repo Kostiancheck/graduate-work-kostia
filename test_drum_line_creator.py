b45b7debd40fcfedd25ce7bd0ee9c0f0256ad185
kostia_horbach <kostiantyn.horbach-ae@transvoyant.com>
2021-08-05 17:08:24 +0300

    Initial commit

---
+++ test_drum_line_creator.py
@@ -0,0 +1,6 @@
+import glob
+import mido
+
+midi_files = glob.glob("groove-v1.0.0-midionly/groove" + '/**/*.mid', recursive=True)
+midi_file_test = mido.MidiFile(midi_files[0], clip=True)
+print(midi_file_test)
